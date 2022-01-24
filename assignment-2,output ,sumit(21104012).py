03d4dbe22ba1bb01386c4a717be6d3f39075b0a9
sumhit <97536414+sumhit@users.noreply.github.com>
2022-01-24 14:44:30 +0530

    Add files via upload

---
+++ assignment-2,output ,sumit(21104012).py
@@ -0,0 +1,70 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/Sumit/Desktop/assg 2 python.py ===============
+answer 1
+python is a case sensitive language
+
+ (a)
+length of string is: 35
+
+ (b)
+reversed string: egaugnal evitisnes esac a si nohtyp
+
+ (c)
+new string: case sensitive language
+
+ (d)
+python is object oriented language
+
+ (e)
+index of substring a: 10
+
+ (f)
+pythonisacasesensitivelanguage
+
+=============== RESTART: C:/Users/Sumit/Desktop/assg 2 python.py ===============
+answer 2
+enter your name:SUMIT KUMAR
+enter your SID:21104012
+department name:EE
+enter your CGPA:9.9
+hey,SUMIT KUMAR here!
+My SID is 21104012
+I am from EE department and my CGPA is 9.9
+
+=============== RESTART: C:/Users/Sumit/Desktop/assg 2 python.py ===============
+
+ answer 3
+
+ a&b is: 8
+
+ a|b is: 58
+
+ a^b is: 50
+
+ a<<2: 224  , b<<2: 40
+
+ a>>2: 14  , b>>4: 0
+
+=============== RESTART: C:/Users/Sumit/Desktop/assg 2 python.py ===============
+
+ answer 4
+first number:354
+second number:654
+third number:236
+second is the greatest of three numbers
+
+=============== RESTART: C:/Users/Sumit/Desktop/assg 2 python.py ===============
+
+ answer 5 
+enter the string:sumit is the name of student
+Yes
+
+=============== RESTART: C:/Users/Sumit/Desktop/assg 2 python.py ===============
+
+ answer 6
+enter the first number:548
+enter the second number:472
+enter the third number:329
+triangle is possible
---
+++ assignment-2,source code,, sumit.py
@@ -0,0 +1,93 @@
+print("answer 1")
+string="python is a case sensitive language"
+print(string)
+
+print("\n (a)")
+#length of the given sentance
+print('length of string is:',len(string))
+
+print("\n (b)")
+#reversed string
+print("reversed string:",string[::-1])
+
+print('\n (c)')
+p=(string[12:35])
+print('new string:',p)
+
+print('\n (d)')
+print(string.replace('a case sensitive' , 'object oriented'))
+
+print("\n (e)")
+print("index of substring a:",string.find("a"))
+
+print("\n (f)")
+#removing white spaces from the string
+print(string.replace(" ",""))
+
+
+print("answer 2")
+p=str(input("enter your name:"))
+q=int(input("enter your SID:"))
+r=str(input("department name:"))
+s=float(input("enter your CGPA:"))
+print('hey,'+p+' here!')
+print('My SID is '+str(q))
+print('I am from '+r+' department and my CGPA is '+str(s)) 
+
+
+
+print("\n answer 3")
+#bitwise operator
+#      32 16 8 4 2 1
+#a=56   1  1 1 0 0 0
+#b=10   0  0 1 0 1 0
+a=56
+b=10
+print('\n a&b is:',a&b)
+print('\n a|b is:',a|b)
+print('\n a^b is:',a^b)
+print('\n a<<2:',a<<2,' , b<<2:',b<<2)
+print('\n a>>2:',a>>2,' , b>>4:',b>>4)
+
+
+
+print("\n answer 4")
+a=int(input("first number:"))
+b=int(input("second number:"))
+c=int(input("third number:"))
+if a>b and a>c :
+   print('first is the greatest of three numbers')
+elif b>c:
+    print('second is the greatest of three numbers')
+else:
+    print('third is the greatest of three numbers')
+
+
+
+
+print("\n answer 5 ")
+x=str(input("enter the string:"))
+if "name" in x:
+    print("Yes")
+else:
+    print("No")
+
+
+print("\n answer 6")
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+if a>b+c:
+      print("triangle is not possible")
+elif b>a+c:
+    print("triangle is not possible")
+elif c>a+b:
+    print("triangle is not possible")
+else:
+    print("triangle is possible")
+
+   
+
+
+
+
