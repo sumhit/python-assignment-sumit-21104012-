28988450df6bcf294c0bd3ee336183204416afec
sumhit <97536414+sumhit@users.noreply.github.com>
2022-02-27 23:57:13 +0530

    Add files via upload

---
+++ assignment 4  output.py
@@ -0,0 +1,68 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:/Users/Sumit/Desktop/python ass4.py ================
+Move disk 1 from disk A to disk C
+Move disk 2 from disk A to disk B
+Move disk 1 from disk C to disk B
+Move disk 3 from disk A to disk C
+Move disk 1 from disk B to disk A
+Move disk 2 from disk B to disk C
+Move disk 1 from disk A to disk C
+
+================ RESTART: C:/Users/Sumit/Desktop/python ass4.py ================
+Enter the Number 6
+BY USING FOR LOOP
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+USING WHILE LOOP
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+RECURSSIONS
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+================ RESTART: C:/Users/Sumit/Desktop/python ass4.py ================
+Enter 2 numbers: 65 39
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> All the values are non zero
+<c> Filtered out numbers that are greater than 4 are : [5, 30, 31, 6, 31, 7, 32]
+<d> Set: {32, 5, 6, 7, 30, 31}
+<e> Immutable set: frozenset({32, 5, 6, 7, 30, 31})
+<f> Hash value of the max value from the above set: 32
+
+========================================================== RESTART: C:/Users/Sumit/Desktop/python ass4.py =========================================================
+Object created
+The name of the student is sumit  and SID is 21104012
+Object destroyed
+
+========================================================== RESTART: C:/Users/Sumit/Desktop/python ass4.py =========================================================
+(a) The updated salary of sumit is 90000
+(b) Record of Varun deleted
+
+========================================================== RESTART: C:/Users/Sumit/Desktop/python ass4.py =========================================================
+Enter the word: 
+sumit
+Enter a new meaningful word using the exact same letters to test your friendship: 
+samit
+The letters aren't exact, friendship is fake
